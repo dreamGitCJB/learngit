4bc4b417be33a48fc985edcd89951d04ce70c023
chenjinbao <569527717@qq.com>
2016-09-09 13:13:15 +0800

    添加了照片和启动页广告

--- News/News/AppDelegate.m
+++ News/News/AppDelegate.m
@@ -7,7 +7,19 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
+#import "XHLaunchAd.h"
 
+#define SCREEN [[UIScreen mainScreen] bounds]
+
+/**
+ *  静态广告
+ */
+#define ImgUrlString1 @"http://d.hiphotos.baidu.com/image/pic/item/14ce36d3d539b60071473204e150352ac75cb7f3.jpg"
+/**
+ *  动态广告
+ */
+#define ImgUrlString2 @"http://c.hiphotos.baidu.com/image/pic/item/d62a6059252dd42a6a943c180b3b5bb5c8eab8e7.jpg"
 @interface AppDelegate ()
 
 @end
@@ -16,8 +28,67 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds
+                   ]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self example];
+    
+    [self.window makeKeyWindow];
     // Override point for customization after application launch.
     return YES;
+}
+/**
+ *  启动页广告
+ */
+- (void)example {
+    [XHLaunchAd showWithAdFrame:CGRectMake(0, 0,SCREEN.size.width,SCREEN.size.height * 80 / 100) setAdImage:^(XHLaunchAd *launchAd) {
+        
+        //未检测到广告数据,启动页停留时间,不设置默认为3,(设置4即表示:启动页显示了4s,还未检测到广告数据,就自动进入window根控制器)
+        //launchAd.noDataDuration = 4;
+        
+        //获取广告数据
+        [self requestImageData:^(NSString *imgUrl, NSInteger duration, NSString *openUrl) {
+            
+            /**
+             *  2.设置广告数据
+             */
+            [launchAd setImageUrl:imgUrl duration:duration skipType:SkipTypeText options:XHWebImageDefault completed:^(UIImage *image, NSURL *url) {
+                
+                //异步加载图片完成回调(若需根据图片尺寸,刷新广告frame,可在这里操作)
+                //launchAd.adFrame = ...;
+                
+            } click:^{
+                
+                //广告点击事件
+                [[UIApplication sharedApplication] openURL:[NSURL URLWithString:openUrl]];
+                
+            }];
+            
+        }];
+        
+    } showFinish:^{
+        
+        //广告展示完成回调,设置window根控制器
+        self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ViewController alloc] init]];
+        
+    }];
+
+}
+
+/**
+ *  模拟:向服务器请求广告数据
+ *
+ *  @param imageData 回调imageUrl,及停留时间,跳转链接
+ */
+-(void)requestImageData:(void(^)(NSString *imgUrl,NSInteger duration,NSString *openUrl))imageData{
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+        if(imageData)
+        {
+            imageData(ImgUrlString2,3,@"http://www.returnoc.com");
+        }
+    });
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
